635e64582e56e354572fd02b8ad26f3b71385da5
timmmoore <tim@youngmoores.com>
2019-06-16 00:27:42 -0700

    Update boards.h

--- Marlin/src/core/boards.h
+++ Marlin/src/core/boards.h
@@ -73,6 +73,7 @@
 #define BOARD_AZTEEG_X3         67    // Azteeg X3
 #define BOARD_AZTEEG_X3_PRO     68    // Azteeg X3 Pro
 #define BOARD_ULTIMAIN_2        72    // Ultimainboard 2.x (Uses TEMP_SENSOR 20)
+#define BOARD_OVERLORD          720   // Overlord Pro
 #define BOARD_RUMBA             80    // Rumba
 #define BOARD_RUMBA_RAISE3D     333   // Raise3D N series Rumba derivative
 #define BOARD_RL200             801   // Rapide Lite 200 (v1, low-cost RUMBA clone with drv)
