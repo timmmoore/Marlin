c3771c1ce6e8dc0f60eabfc1c2f9410632a379bf
timmmoore <tim@youngmoores.com>
2019-06-22 15:10:24 -0700

    Increase probe low

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1016,7 +1016,7 @@
 #define Z_CLEARANCE_MULTI_PROBE    15 // Z Clearance between multiple probes
 #define Z_AFTER_PROBING            15 // Z position after probing is done
 
-#define Z_PROBE_LOW_POINT          -2 // Farthest distance below the trigger-point to go before stopping
+#define Z_PROBE_LOW_POINT          -4 // Farthest distance below the trigger-point to go before stopping
 
 // For M851 give a range for adjusting the Z probe offset
 #define Z_PROBE_OFFSET_RANGE_MIN -20
