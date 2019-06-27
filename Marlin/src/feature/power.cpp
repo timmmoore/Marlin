01821f787a07ff4a6eb59c4e4131051873218e61
Scott Lahteine <github@thinkyhead.com>
2019-06-27 15:54:04 -0500

    Apply new auto power control options

--- Marlin/src/feature/power.cpp
+++ Marlin/src/feature/power.cpp
@@ -94,6 +94,14 @@ bool Power::is_power_needed() {
     if (thermalManager.degTargetBed() > 0) return true;
   #endif
 
+  #if HOTENDS && ENABLED(AUTO_POWER_E_TEMP)
+    HOTEND_LOOP() if (thermalManager.degHotend(e) >= EXTRUDER_AUTO_FAN_TEMPERATURE) return true;
+  #endif
+
+  #if HAS_HEATED_CHAMBER && ENABLED(AUTO_POWER_CHAMBER_TEMP)
+    if (thermalManager.degChamber() >= CHAMBER_AUTO_FAN_TEMPERATURE) return true;
+  #endif
+
   return false;
 }
 
