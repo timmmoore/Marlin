b4eade2f92a56c9c3339c4cf4cc0824fdc76572e
timmmoore <tim@youngmoores.com>
2019-07-02 16:14:23 -0700

    simipify power supply name

--- Marlin/src/lcd/menu/menu_info.cpp
+++ Marlin/src/lcd/menu/menu_info.cpp
@@ -177,20 +177,14 @@ void menu_info_board() {
   STATIC_ITEM(BOARD_NAME, true, true);                           // MyPrinterController
   STATIC_ITEM(MSG_INFO_BAUDRATE ": " STRINGIFY(BAUDRATE), true); // Baud: 250000
   STATIC_ITEM(MSG_INFO_PROTOCOL ": " PROTOCOL_VERSION, true);    // Protocol: 1.0
-  #if POWER_SUPPLY == 0
+  #if defined(POWER_SUPPLY_NAME)
+    STATIC_ITEM(MSG_INFO_PSU ": " POWER_SUPPLY_NAME, true);
+  #elif POWER_SUPPLY == 0
     STATIC_ITEM(MSG_INFO_PSU ": Generic", true);
   #elif POWER_SUPPLY == 1
-    #if defined(POWER_SUPPLY_NAME)
-      STATIC_ITEM(MSG_INFO_PSU ": " POWER_SUPPLY_NAME, true);
-    #else
-      STATIC_ITEM(MSG_INFO_PSU ": ATX", true);  // Power Supply: ATX
-    #endif
+    STATIC_ITEM(MSG_INFO_PSU ": ATX", true);  // Power Supply: ATX
   #elif POWER_SUPPLY == 2
-    #if defined(POWER_SUPPLY_NAME)
-      STATIC_ITEM(MSG_INFO_PSU ": " POWER_SUPPLY_NAME, true);
-    #else
-      STATIC_ITEM(MSG_INFO_PSU ": XBox", true);     // Power Supply: XBox
-    #endif
+    STATIC_ITEM(MSG_INFO_PSU ": XBox", true);     // Power Supply: XBox
   #endif
   #if ENABLED(BATTERY_STATUS_AVAILABLE) && PIN_EXISTS(BATTERY_STATUS)
     if (READ(BATTERY_STATUS_PIN) != BATTERY_STATUS_CHARGED)
