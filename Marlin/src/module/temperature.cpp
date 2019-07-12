e16e5aaeaf51956424555155288d88e33f708261
timmmoore <tim@youngmoores.com>
2019-07-12 10:25:48 -0700

    no bed when hotend is one

--- Marlin/src/module/temperature.cpp
+++ Marlin/src/module/temperature.cpp
@@ -2360,6 +2360,7 @@ void Temperature::isr() {
   static uint8_t pwm_count = _BV(SOFT_PWM_SCALE);
   // avoid multiple loads of pwm_count
   uint8_t pwm_count_tmp = pwm_count;
+  bool stop_bed = false;
 
   #if HAS_ADC_BUTTONS
     static unsigned int raw_ADCKey_value = 0;
@@ -2399,18 +2400,24 @@ void Temperature::isr() {
     if (pwm_count_tmp >= 127) {
       pwm_count_tmp -= 127;
       #if ENABLED(BED_HOTEND_ONE)
-        #define _PWM_MOD(N,S,T) do{                           \
+        #define _PWM_MOD_B(N,S,T) do{                         \
           const bool on = S.add(pwm_mask, T.soft_pwm_amount); \
-          if(on) WRITE_HEATER_BED(LOW);                       \
+          if(!stop_bed) WRITE_HEATER_##N(on);                 \
+        }while(0)
+        #define _PWM_MOD_EE(N,S,T) do{                        \
+          const bool on = S.add(pwm_mask, T.soft_pwm_amount); \
+          if(on) { WRITE_HEATER_BED(LOW); stop_bed = true; }  \
           WRITE_HEATER_##N(on);                               \
         }while(0)
+        #define _PWM_MOD_E(N) _PWM_MOD_EE(N,soft_pwm_hotend[N],temp_hotend[N])
+        #define _PWM_MOD(N,S,T) _PWM_MOD_B(N,S,T)
       #else
         #define _PWM_MOD(N,S,T) do{                           \
           const bool on = S.add(pwm_mask, T.soft_pwm_amount); \
           WRITE_HEATER_##N(on);                               \
         }while(0)
+        #define _PWM_MOD_E(N) _PWM_MOD(N,soft_pwm_hotend[N],temp_hotend[N])
       #endif
-      #define _PWM_MOD_E(N) _PWM_MOD(N,soft_pwm_hotend[N],temp_hotend[N])
       _PWM_MOD_E(0);
       #if HOTENDS > 1
         _PWM_MOD_E(1);
