c4e81ed8c6b4574606ef2bde5c7c741ea87132c6
timmmoore <tim@youngmoores.com>
2019-07-14 14:23:59 -0700

    Addional check

--- Marlin/src/pins/mega/pins_OVERLORD.h
+++ Marlin/src/pins/mega/pins_OVERLORD.h
@@ -27,7 +27,9 @@
 
 #if !defined(__AVR_ATmega2560__)
   #error "Oops! Select 'Arduino/Genuino Mega or Mega 2560' in 'Tools > Board.'"
-#endif
+#elif HOTENDS > 2 || E_STEPPERS > 2
+  #error "Overlord Controller supports up to 2 hotends / E-steppers. Comment out this line to continue."
+#endif#endif
 
 #define BOARD_NAME "OVERLORD"
 #define DEFAULT_MACHINE_NAME BOARD_NAME
