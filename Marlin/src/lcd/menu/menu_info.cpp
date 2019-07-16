796803eaaf942a114d9b269bfb69d25a10a8da52
timmmoore <tim@youngmoores.com>
2019-07-16 11:08:22 -0700

    tidy up runaway messages

--- Marlin/src/lcd/menu/menu_info.cpp
+++ Marlin/src/lcd/menu/menu_info.cpp
@@ -156,7 +156,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_5_MAXTEMP), false);
   #endif
   #if WATCH_HOTENDS
-    STATIC_ITEM_P(PSTR(MSG_THERMAL_RUNAWAY_ON), false, true);
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_ON, false, true);
   #endif
 
   #if HAS_HEATED_BED
@@ -168,7 +168,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(BED_MAXTEMP), false);
   #endif
   #if WATCH_BED
-    STATIC_ITEM_P(PSTR(MSG_THERMAL_RUNAWAY_BED_ON), false, true);
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_BED_ON, false, true);
   #endif
 
   #if HAS_HEATED_CHAMBER
@@ -180,7 +180,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(CHAMBER_MAXTEMP), false);
   #endif
   #if WATCH_CHAMBER
-    STATIC_ITEM_P(PSTR(MSG_THERMAL_RUNAWAY_CHAMBER_ON), false, true);
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_CHAMBER_ON, false, true);
   #endif
   END_SCREEN();
 }
@@ -220,7 +220,7 @@ void menu_info_power() {
   #if HAS_VOLTAGE_AVAILABLE
     #if HAS_POWER_SWITCH && DISABLED(VOLTAGE_ALWAYS_AVAILABLE)
       if (!powersupply_on)
-        STATIC_ITEM_P(PSTR("Power Voltage: OFF"), true);
+        STATIC_ITEM("Power Voltage: OFF", true);
       else
     #endif
       {
