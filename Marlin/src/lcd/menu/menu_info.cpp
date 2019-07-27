5f32c8db8d31410af5fe01bd05084e139f1b5db4
Tim Moore <tim@youngmoores.com>
2019-07-26 20:32:05 -0700

    Voltage and battery display and voltage too low check

--- Marlin/src/lcd/menu/menu_info.cpp
+++ Marlin/src/lcd/menu/menu_info.cpp
@@ -29,6 +29,7 @@
 #if HAS_LCD_MENU && ENABLED(LCD_INFO_MENU)
 
 #include "menu.h"
+#include "../../module/temperature.h"
 
 #if HAS_GAMES
   #include "game/game.h"
@@ -147,6 +148,9 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(HEATER_5_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_5_MAXTEMP), false);
   #endif
+  #if WATCH_HOTENDS
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_ON, false, true);
+  #endif
 
   #if HAS_HEATED_BED
     #undef THERMISTOR_ID
@@ -155,6 +159,21 @@ void menu_info_thermistors() {
     STATIC_ITEM("TBed:" THERMISTOR_NAME, false, true);
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(BED_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(BED_MAXTEMP), false);
+  #endif
+  #if WATCH_BED
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_BED_ON, false, true);
+  #endif
+
+  #if HAS_HEATED_CHAMBER
+    #undef THERMISTOR_ID
+    #define THERMISTOR_ID TEMP_SENSOR_CHAMBER
+    #include "../thermistornames.h"
+    STATIC_ITEM("TCham:" THERMISTOR_NAME, false, true);
+    STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(CHAMBER_MINTEMP), false);
+    STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(CHAMBER_MAXTEMP), false);
+  #endif
+  #if WATCH_CHAMBER
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_CHAMBER_ON, false, true);
   #endif
   END_SCREEN();
 }
@@ -168,7 +187,43 @@ void menu_info_board() {
   STATIC_ITEM(BOARD_NAME, true, true);                           // MyPrinterController
   STATIC_ITEM(MSG_INFO_BAUDRATE ": " STRINGIFY(BAUDRATE), true); // Baud: 250000
   STATIC_ITEM(MSG_INFO_PROTOCOL ": " PROTOCOL_VERSION, true);    // Protocol: 1.0
+  END_SCREEN();
+}
+
+//
+// About Printer > Power Info
+//
+void menu_info_power() {
+  if (ui.use_click()) return ui.goto_previous_screen();
+  START_SCREEN();
   STATIC_ITEM(MSG_INFO_PSU ": " PSU_NAME, true);
+  #if ENABLED(POWER_LOSS_RECOVERY)
+    #if PIN_EXISTS(POWER_LOSS)
+      STATIC_ITEM(MSG_INFO_POWER_LOSS ": Power", true);
+    #else
+      STATIC_ITEM(MSG_INFO_POWER_LOSS ": " STRINGIFY(POWER_LOSS_MIN_Z_CHANGE) "mm", true);
+    #endif
+  #endif
+  #if HAS_BATTERY_STATUS
+    if (READ(BATTERY_STATUS_PIN) != BATTERY_STATUS_CHARGED)
+      STATIC_ITEM("Battery" ": Charging", true);
+    else
+      STATIC_ITEM("Battery" ": Charged", true);
+  #endif
+  #if HAS_VOLTAGE_AVAILABLE
+    #if HAS_POWER_SWITCH && DISABLED(VOLTAGE_ALWAYS_AVAILABLE)
+      if (!powersupply_on)
+        STATIC_ITEM("Power Voltage: OFF", true);
+      else
+    #endif
+      {
+        char buffer[8];
+        uint16_t volt;
+        volt = (uint16_t)(((float)thermalManager.voltage_level * DIVIDER_RATIO) + 0.5f);
+        sprintf_P(buffer, PSTR("%3d.%02dV"), volt / 100, volt % 100);
+        STATIC_ITEM_P(PSTR("Power Voltage: "), false, false, buffer);
+      }
+  #endif
   END_SCREEN();
 }
 
@@ -229,6 +284,7 @@ void menu_info() {
   #else
     MENU_ITEM(submenu, MSG_INFO_PRINTER_MENU, menu_info_printer);        // Printer Info >
     MENU_ITEM(submenu, MSG_INFO_BOARD_MENU, menu_info_board);            // Board Info >
+    MENU_ITEM(submenu, MSG_INFO_POWER_MENU, menu_info_power);            // Power Info >
     MENU_ITEM(submenu, MSG_INFO_THERMISTOR_MENU, menu_info_thermistors); // Thermistors >
   #endif
 
