3a9387cf6d195d495b4f721c432f2f0cccf66723
Tim Moore <tim@youngmoores.com>
2019-08-03 11:04:07 -0700

    Add chanber thermistor info  and runaway on/off to menu_info

--- Marlin/src/lcd/menu/menu_info.cpp
+++ Marlin/src/lcd/menu/menu_info.cpp
@@ -147,6 +147,9 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(HEATER_5_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_5_MAXTEMP), false);
   #endif
+  #if WATCH_HOTENDS
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_ON, false, true);
+  #endif
 
   #if HAS_HEATED_BED
     #undef THERMISTOR_ID
@@ -155,6 +158,21 @@ void menu_info_thermistors() {
     STATIC_ITEM("TBed:" THERMISTOR_NAME, false, true);
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(BED_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(BED_MAXTEMP), false);
+  #endif
+  #if WATCH_BED
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_BED_ON, false, true);
+  #endif
+
+  #if HAS_HEATED_CHAMBER
+    #undef THERMISTOR_ID
+    #define THERMISTOR_ID TEMP_SENSOR_CHAMBER
+    #include "../thermistornames.h"
+    STATIC_ITEM("TCham:" THERMISTOR_NAME, false, true);
+    STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(CHAMBER_MINTEMP), false);
+    STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(CHAMBER_MAXTEMP), false);
+  #endif
+  #if WATCH_CHAMBER
+    STATIC_ITEM(MSG_THERMAL_RUNAWAY_CHAMBER_ON, false, true);
   #endif
   END_SCREEN();
 }
