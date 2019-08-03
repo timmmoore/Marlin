affd9a2f78d00c646e530d1231b7022eb024bbf4
Scott Lahteine <github@thinkyhead.com>
2019-08-03 18:08:07 -0500

    Show on or off state

--- Marlin/src/lcd/menu/menu_info.cpp
+++ Marlin/src/lcd/menu/menu_info.cpp
@@ -97,13 +97,16 @@
 void menu_info_thermistors() {
   if (ui.use_click()) return ui.goto_previous_screen();
   START_SCREEN();
-  #define THERMISTOR_ID TEMP_SENSOR_0
-  #include "../thermistornames.h"
-  STATIC_ITEM("T0: " THERMISTOR_NAME, false, true);
-  STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(HEATER_0_MINTEMP), false);
-  STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_0_MAXTEMP), false);
 
-  #if TEMP_SENSOR_1 != 0
+  #if TEMP_SENSOR_0
+    #define THERMISTOR_ID TEMP_SENSOR_0
+    #include "../thermistornames.h"
+    STATIC_ITEM("T0: " THERMISTOR_NAME, false, true);
+    STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(HEATER_0_MINTEMP), false);
+    STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_0_MAXTEMP), false);
+  #endif
+
+  #if TEMP_SENSOR_1
     #undef THERMISTOR_ID
     #define THERMISTOR_ID TEMP_SENSOR_1
     #include "../thermistornames.h"
@@ -112,7 +115,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_1_MAXTEMP), false);
   #endif
 
-  #if TEMP_SENSOR_2 != 0
+  #if TEMP_SENSOR_2
     #undef THERMISTOR_ID
     #define THERMISTOR_ID TEMP_SENSOR_2
     #include "../thermistornames.h"
@@ -121,7 +124,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_2_MAXTEMP), false);
   #endif
 
-  #if TEMP_SENSOR_3 != 0
+  #if TEMP_SENSOR_3
     #undef THERMISTOR_ID
     #define THERMISTOR_ID TEMP_SENSOR_3
     #include "../thermistornames.h"
@@ -130,7 +133,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_3_MAXTEMP), false);
   #endif
 
-  #if TEMP_SENSOR_4 != 0
+  #if TEMP_SENSOR_4
     #undef THERMISTOR_ID
     #define THERMISTOR_ID TEMP_SENSOR_4
     #include "../thermistornames.h"
@@ -139,7 +142,7 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_4_MAXTEMP), false);
   #endif
 
-  #if TEMP_SENSOR_5 != 0
+  #if TEMP_SENSOR_5
     #undef THERMISTOR_ID
     #define THERMISTOR_ID TEMP_SENSOR_5
     #include "../thermistornames.h"
@@ -147,8 +150,16 @@ void menu_info_thermistors() {
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(HEATER_5_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(HEATER_5_MAXTEMP), false);
   #endif
-  #if WATCH_HOTENDS
-    STATIC_ITEM(MSG_THERMAL_RUNAWAY_ON, false, true);
+
+  #if EXTRUDERS
+    STATIC_ITEM(
+      #if WATCH_HOTENDS
+        MSG_INFO_RUNAWAY_ON
+      #else
+        MSG_INFO_RUNAWAY_OFF
+      #endif
+      , false, true
+    );
   #endif
 
   #if HAS_HEATED_BED
@@ -158,9 +169,14 @@ void menu_info_thermistors() {
     STATIC_ITEM("TBed:" THERMISTOR_NAME, false, true);
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(BED_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(BED_MAXTEMP), false);
-  #endif
-  #if WATCH_BED
-    STATIC_ITEM(MSG_THERMAL_RUNAWAY_BED_ON, false, true);
+    STATIC_ITEM(
+      #if WATCH_BED
+        MSG_INFO_RUNAWAY_ON
+      #else
+        MSG_INFO_RUNAWAY_OFF
+      #endif
+      , false, true
+    );
   #endif
 
   #if HAS_HEATED_CHAMBER
@@ -170,9 +186,14 @@ void menu_info_thermistors() {
     STATIC_ITEM("TCham:" THERMISTOR_NAME, false, true);
     STATIC_ITEM(MSG_INFO_MIN_TEMP ": " STRINGIFY(CHAMBER_MINTEMP), false);
     STATIC_ITEM(MSG_INFO_MAX_TEMP ": " STRINGIFY(CHAMBER_MAXTEMP), false);
-  #endif
-  #if WATCH_CHAMBER
-    STATIC_ITEM(MSG_THERMAL_RUNAWAY_CHAMBER_ON, false, true);
+    STATIC_ITEM(
+      #if WATCH_CHAMBER
+        MSG_INFO_RUNAWAY_ON
+      #else
+        MSG_INFO_RUNAWAY_OFF
+      #endif
+      , false, true
+    );
   #endif
   END_SCREEN();
 }
