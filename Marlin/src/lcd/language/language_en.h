a58c377f6e7aeb9505d4f03ab685b06b33aa87a7
Tim Moore <tim@youngmoores.com>
2019-08-11 19:01:17 -0700

    Add display of power loss pin state to M413 and filament sensor state to M412

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -1537,3 +1537,13 @@
 #ifndef MSG_BACKLASH_SMOOTHING
   #define MSG_BACKLASH_SMOOTHING              _UxGT("Smoothing")
 #endif
+
+#ifndef MSG_ENDSTOP_OK
+  #define MSG_ENDSTOP_OK                      _UxGT("ok")
+#endif
+#ifndef MSG_POWER_OFF
+  #define MSG_POWER_OFF                       _UxGT("lost")
+#endif
+#ifndef MSG_FILAMENT_OPEN
+  #define MSG_FILAMENT_OPEN                   _UxGT("None")
+#endif
