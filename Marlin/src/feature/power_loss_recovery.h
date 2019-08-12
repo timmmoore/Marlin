976506882e268cdbeb05d6096ade72e07fa34bc3
Tim Moore <tim@youngmoores.com>
2019-08-11 19:01:17 -0700

    Add display of power loss pin state to M413 and filament sensor state to M412

--- Marlin/src/feature/power_loss_recovery.h
+++ Marlin/src/feature/power_loss_recovery.h
@@ -139,6 +139,8 @@ class PrintJobRecovery {
     static inline void open(const bool read) { card.openJobRecoveryFile(read); }
     static inline void close() { file.close(); }
 
+    static inline bool read() { return(READ(POWER_LOSS_PIN) == POWER_LOSS_STATE);}
+
     static void purge();
     static void load();
     static void save(const bool force=
