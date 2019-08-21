ba15dd1107419a4da6f93603cd2bdd356193d0c7
Tim Moore <tim@youngmoores.com>
2019-08-21 13:19:29 -0700

    Make endstop user display optional

--- Marlin/src/module/endstops.cpp
+++ Marlin/src/module/endstops.cpp
@@ -384,10 +384,10 @@ void Endstops::event_handler() {
   prev_hit_state = hit_state;
 }
 
-static void print_es_state(const bool is_hit, PGM_P const label=nullptr, const uint8_t type=0) {
+static void print_es_state(const bool is_hit, PGM_P const label=nullptr, const uint8_t type=0, const bool display=false) {
   if (label) serialprintPGM(label);
   SERIAL_ECHOPGM(": ");
-  if(type == 0)           // endstop
+  if((type == 0) || (display==false))           // endstop
     serialprintPGM(is_hit ? PSTR(MSG_ENDSTOP_HIT) : PSTR(MSG_ENDSTOP_OPEN));
   else if(type == 1)      // filament
     serialprintPGM(is_hit ? PSTR(MSG_FILAMENT_HIT) : PSTR(MSG_FILAMENT_OPEN));
@@ -398,12 +398,12 @@ static void print_es_state(const bool is_hit, PGM_P const label=nullptr, const u
   SERIAL_EOL();
 }
 
-void _O2 Endstops::M119() {
+void _O2 Endstops::M119(const bool display) {
   #if ENABLED(BLTOUCH)
     bltouch._set_SW_mode();
   #endif
   SERIAL_ECHOLNPGM(MSG_M119_REPORT);
-  #define ES_REPORT(S) print_es_state(READ(S##_PIN) != S##_ENDSTOP_INVERTING, PSTR(MSG_##S))
+  #define ES_REPORT(S) print_es_state(READ(S##_PIN) != S##_ENDSTOP_INVERTING, PSTR(MSG_##S), display)
   #if HAS_X_MIN
     ES_REPORT(X_MIN);
   #endif
@@ -430,7 +430,7 @@ void _O2 Endstops::M119() {
   #endif
   #if HAS_Z_MIN
     #if ENABLED(Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN)
-      print_es_state(READ(Z_MIN_PIN) != Z_MIN_PROBE_ENDSTOP_INVERTING, PSTR(MSG_Z_PROBE), 2);
+      print_es_state(READ(Z_MIN_PIN) != Z_MIN_PROBE_ENDSTOP_INVERTING, PSTR(MSG_Z_PROBE), 2, display);
     #else
       ES_REPORT(Z_MIN);
     #endif
@@ -451,11 +451,11 @@ void _O2 Endstops::M119() {
     ES_REPORT(Z3_MAX);
   #endif
   #if USES_Z_MIN_PROBE_ENDSTOP
-    print_es_state(READ(Z_MIN_PROBE_PIN) != Z_MIN_PROBE_ENDSTOP_INVERTING, PSTR(MSG_Z_PROBE), 2);
+    print_es_state(READ(Z_MIN_PROBE_PIN) != Z_MIN_PROBE_ENDSTOP_INVERTING, PSTR(MSG_Z_PROBE), 2, display);
   #endif
   #if HAS_FILAMENT_SENSOR
     #if NUM_RUNOUT_SENSORS == 1
-      print_es_state(READ(FIL_RUNOUT_PIN) != FIL_RUNOUT_INVERTING, PSTR(MSG_FILAMENT_RUNOUT_SENSOR), 1);
+      print_es_state(READ(FIL_RUNOUT_PIN) != FIL_RUNOUT_INVERTING, PSTR(MSG_FILAMENT_RUNOUT_SENSOR), 1, display);
     #else
       for (uint8_t i = 1; i <= NUM_RUNOUT_SENSORS; i++) {
         pin_t pin;
@@ -478,12 +478,12 @@ void _O2 Endstops::M119() {
         }
         SERIAL_ECHOPGM(MSG_FILAMENT_RUNOUT_SENSOR);
         if (i > 1) { SERIAL_CHAR(' '); SERIAL_CHAR('0' + i); }
-        print_es_state(extDigitalRead(pin) != FIL_RUNOUT_INVERTING, nullptr, 1);
+        print_es_state(extDigitalRead(pin) != FIL_RUNOUT_INVERTING, nullptr, 1, display);
       }
     #endif
   #endif
   #if ENABLED(POWER_LOSS_RECOVERY) && PIN_EXISTS(POWER_LOSS)
-    print_es_state(READ(POWER_LOSS_PIN) != POWER_LOSS_STATE, PSTR(MSG_POWER_LOSS), 3);
+    print_es_state(READ(POWER_LOSS_PIN) != POWER_LOSS_STATE, PSTR(MSG_POWER_LOSS), 3, display);
   #endif
   #if ENABLED(BLTOUCH)
     bltouch._reset_SW_mode();
