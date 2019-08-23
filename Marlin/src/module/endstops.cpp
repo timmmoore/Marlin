fff2409af3d0be28fdf3698f81e0a5c63f6c8374
Tim Moore <tim@youngmoores.com>
2019-08-22 22:41:30 -0700

    tidy comments

--- Marlin/src/module/endstops.cpp
+++ Marlin/src/module/endstops.cpp
@@ -389,8 +389,8 @@ static void print_es_state(const bool is_hit, PGM_P const label=nullptr, const u
   SERIAL_ECHOPGM(": ");
   switch (type) {
     case 0: serialprintPGM(is_hit ? PSTR(MSG_ENDSTOP_HIT) : PSTR(MSG_ENDSTOP_OPEN)); break;         // default non localized strings
-    case 1: serialprintPGM(is_hit ? PSTR(MSG_FILAMENT_HIT) : PSTR(MSG_FILAMENT_OPEN)); break;   // filament
-    case 2: serialprintPGM(is_hit ? PSTR(MSG_PROBE_HIT) : PSTR(MSG_PROBE_OPEN)); break;         // probe
+    case 1: serialprintPGM(is_hit ? PSTR(MSG_FILAMENT_HIT) : PSTR(MSG_FILAMENT_OPEN)); break;       // filament
+    case 2: serialprintPGM(is_hit ? PSTR(MSG_PROBE_HIT) : PSTR(MSG_PROBE_OPEN)); break;             // probe
     case 3: serialprintPGM(is_hit ? PSTR(MSG_LOC_ENDSTOP_HIT) : PSTR(MSG_LOC_ENDSTOP_OPEN)); break; // endstop
     default: serialprintPGM(is_hit ? PSTR(MSG_POWER_ON) : PSTR(MSG_POWER_OFF));                     // other
   }
