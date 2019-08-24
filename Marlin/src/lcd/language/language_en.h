f559667d353f6b2571613d56d8cd5ee1c5aad271
Tim Moore <tim@youngmoores.com>
2019-08-24 11:19:43 -0700

    missing string

--- Marlin/src/lcd/language/language_en.h
+++ Marlin/src/lcd/language/language_en.h
@@ -1550,3 +1550,6 @@
 #ifndef MSG_FILAMENT_SENSOR
   #define MSG_FILAMENT_SENSOR                 _UxGT("Filament sensor")
 #endif
+#ifndef MSG_POWER_LOSS
+  #define MSG_POWER_LOSS                      _UxGT("Power")
+#endif
