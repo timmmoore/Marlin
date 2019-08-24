2d84ebcdaaa47181da3a92301d7cc13a918b1d66
Tim Moore <tim@youngmoores.com>
2019-08-23 19:09:11 -0700

    Update M413.cpp

--- Marlin/src/gcode/feature/powerloss/M413.cpp
+++ Marlin/src/gcode/feature/powerloss/M413.cpp
@@ -45,7 +45,7 @@ void GcodeSuite::M413() {
     SERIAL_ECHOPGM("Power-loss recovery ");
     serialprintln_onoff(recovery.enabled);
     SERIAL_ECHOPGM(MSG_POWER_LOSS " ");
-    serialprintPGM(recovery.read() ? PSTR(MSG_ENDSTOP_OK) : PSTR(MSG_POWER_OFF));
+    serialprintPGM(!recovery.read() ? PSTR(MSG_ENDSTOP_OK) : PSTR(MSG_POWER_OFF));
     SERIAL_EOL();
   }
 
